ce6c9883df5e9ebcd97b8bd3fea82634c3bbcd51
TimofeyRat <timofejhismatulin@gmail.com>
2024-02-23 14:40:01 +0500

    Fixed the bones mentions.

--- mapEditor.cpp
+++ mapEditor.cpp
@@ -1,9 +1,12 @@
 #include "Engine/hWindow.hpp"
+#include "Engine/hWorld.hpp"
 
 int main(int argc, char* argv[])
 {
 	Window::init(argc, argv);
 	Window::setTitle("MapEditor");
+
+	World::init();
 
 	while (Window::isOpen())
 	{
